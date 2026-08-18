1c10f4e02b03f0cb61b6a9735ae9377c19f0ace8
NickCoffin123 <nicholas.coffin@dcmail.ca>
2026-08-18 11:57:26 -0400

    connect reviews page to api

--- client/src/pages/Reviews.tsx
+++ client/src/pages/Reviews.tsx
@@ -1,13 +1,34 @@
 import ReviewCard from "../components/ReviewCard";
-import { reviews } from "../data/reviews";
+import { useState, useEffect } from "react";
+import type { Review } from "../types/Review";
 
 export default function Reviews() {
+
+    const [allReviews, setAllReviews] = useState<Review[]>([]);
+
+    useEffect(() => {
+        const fetchReviews = async () => {
+            try {
+                const response = await fetch("http://localhost:5085/api/reviews")
+
+                const data = await response.json();
+
+                setAllReviews(data);
+
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchReviews();
+
+    }, []);
+
     return (
         <>
             <section className="mx-auto max-w-5xl px-8 py-16">
                 <h1 className="text-5xl font-bold mb-6">Reviews</h1>
 
-                {reviews.map((review) => (
+                {allReviews.map((review) => (
                     <ReviewCard
                         key={review.id}
                         review={review}
--- server/Program.cs
+++ server/Program.cs
@@ -4,6 +4,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactDev", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173");
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -15,6 +24,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.MapControllers();
+app.UseCors("AllowReactDev");
 
 
 app.Run();
